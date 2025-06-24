84ae08c09240c6523b9fafec5f452bdb8b381501
Farooq <2417692@cognizant.com>
2025-06-24 13:58:19 +0100

    LoginApiTest added

---
+++ src/test/java/api/LoginApiTest.java
@@ -0,0 +1,30 @@
+package api;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginApiTest {
+
+    @Test
+    public void testLoginApi() {
+
+        RestAssured.baseURI = "https://demoqa.com";
+
+        String requestBody = "{ \"userName\": \"your-username\", \"password\": \"your-password\" }";
+
+        Response response = RestAssured
+            .given()
+            .header("Content-Type", "application/json")
+            .body(requestBody)
+            .post("/Account/v1/Login");
+
+        int statusCode = response.getStatusCode();
+        System.out.println("Response code: " + statusCode);
+        System.out.println("Response Body: " + response.getBody().asString());
+
+        Assert.assertEquals(statusCode, 200, "Expected status code 200");
+    }
+
+}
